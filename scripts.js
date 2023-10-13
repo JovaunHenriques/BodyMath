f69579c523dc41a65a4203d54603e062b4ba7a49
Jovaun_Henriques <jovaun@hotmail.com>
2023-10-13 09:41:13 -0230

    added the js file

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Get the <a> elements by their respective IDs
+let home =document.getElementById("home");
+let about = document.getElementById("about");
+let contact = document.getElementById("contact");
+let services = document.getElementById("services");
+let = document.getElementById("");
+let = document.getElementById("");
