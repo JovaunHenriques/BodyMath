2030791930d610869de3e4b5875317f41f7ea235
Jovaun_Henriques <jovaun@hotmail.com>
2023-10-18 17:41:55 -0230

    added imgae and js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,21 @@
 // Get the <a> elements by their respective IDs
-let home =document.getElementById("home");
-let about = document.getElementById("about");
+const home = document.getElementById("home");
+const about = document.getElementById("about");
 let contact = document.getElementById("contact");
 let services = document.getElementById("services");
 let = document.getElementById("");
 let = document.getElementById("");
+
+const muscles = document.getElementById('muscles');
+const highlight = document.getElementById('highlight');
+const resources = document.getElementById('resources');
+
+muscles.addEventListener('mouseenter', () => {
+    highlight.style.opacity = 0.6;
+    resources.style.display = 'block';
+});
+
+muscles.addEventListener('mouseleave', () => {
+    highlight.style.opacity = 0;
+    resources.style.display = 'none';
+});
