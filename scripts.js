a6b67598c400610ba93e69ed117c5d8228519c74
Jovaun_Henriques <jovaun@hotmail.com>
2023-10-26 15:16:27 -0230

    edits

--- scripts.js
+++ scripts.js
@@ -21,3 +21,29 @@ const navbar = document.querySelector('.navbar');
 navbar.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
+const registrationForm = document.getElementById("registrationForm");
+const message = document.getElementById("message");
+
+registrationForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    // Get user input
+    const username = registrationForm.username.value;
+    const email = registrationForm.email.value;
+    const password = registrationForm.password.value;
+
+    // Perform client-side validation (you should also do server-side validation)
+    if (username === "" || email === "" || password === "") {
+        message.innerHTML = "Please fill in all fields.";
+        return;
+    }
+
+    // You can now send this data to the server for server-side validation and registration.
+    // Simulate a registration request (replace with actual AJAX request)
+    message.innerHTML = "Registering...";
+
+    // Simulate a successful registration
+    setTimeout(() => {
+        message.innerHTML = "Registration successful!";
+    }, 2000);
+});
