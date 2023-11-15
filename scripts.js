bb20cc062550461dcfdb1d2e587307e12ed97503
Jovaun_Henriques <jovaun@hotmail.com>
2023-11-14 23:39:26 -0330

    edits

--- scripts.js
+++ scripts.js
@@ -1,100 +1,94 @@
-function showLogin_popup(onclick) {
-    let form = document.getElementById("registration")
-    form.classList.remove("form--hidden")
+function showLoginPopup() {
+    let form = document.getElementById("registration");
+    form.classList.remove("form--hidden");
 }
 
-function toggleForm(onclick){
-    let form_2 =document.getElementById("registrationModal")
-    if (window.getComputedStyle(form).display = 'block') {
-        form_2.classList.remove("registrationModal--hidden")
+function toggleForm() {
+    let form_2 = document.getElementById("combinedFormModal");
+    if (window.getComputedStyle(form_2).display === 'block') {
+        form_2.classList.remove("form--hidden");
     }
-
 }
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Add smooth scrolling to navigation links
-    document.querySelectorAll('a').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
+    function addSmoothScrolling() {
+        document.querySelectorAll('a').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
 
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
+                const targetId = this.getAttribute('href').substring(1);
+                const targetElement = document.getElementById(targetId);
 
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop - 50,
-                    behavior: 'smooth'
-                });
-            }
+                if (targetElement) {
+                    window.scrollTo({
+                        top: targetElement.offsetTop - 50,
+                        behavior: 'smooth'
+                    });
+                }
+            });
         });
-    });
+    }
 
     // Event listeners for navigation links
-    const navigateTo = (page) => {
+    function navigateTo(page) {
         window.location.href = `./${page}.html`;
-    };
-
-    document.getElementById("linkedin-Link").addEventListener("click", function () {
-        navigateTo("https://www.linkedin.com/in/jovaun-henriques-37b61b195/");
-    });
-
-    document.getElementById("homePage").addEventListener("click", function () {
-        navigateTo("index");
-    });
-
-    document.getElementById("aboutPage").addEventListener("click", function () {
-        navigateTo("about page");
-    });
-
-    document.getElementById("contactPage").addEventListener("click", function () {
-        navigateTo("contactpage");
-    });
-
-    // Toggle navigation menu for smaller screens
-    const navbar = document.querySelector('.navbar');
-
-    navbar.addEventListener('click', () => {
-        navbar.classList.toggle('active');
-    });
-    function open_login_popup(click){
-
     }
 
-    // Toggle Log In and Sign Up forms
-    const showLoginFormButton = document.getElementById("showLogin");
-    const showCreateAccountFormButton = document.getElementById("showCreateAccount");
-    const loginForm = document.getElementById("loginForm");
-    const createAccountForm = document.getElementById("createAccountForm");
+    function setupNavigationLinks() {
+        document.getElementById("linkedin-Link").addEventListener("click", function () {
+            navigateTo("https://www.linkedin.com/in/jovaun-henriques-37b61b195/");
+        });
 
-    showLoginFormButton.addEventListener("click", () => {
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
+        document.getElementById("homePage").addEventListener("click", function () {
+            navigateTo("index");
+        });
 
-    showCreateAccountFormButton.addEventListener("click", () => {
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
+        document.getElementById("aboutPage").addEventListener("click", function () {
+            navigateTo("about page");
+        });
 
-    // Toggle combined form modal
-    const toggleFormButton = document.getElementById("toggleForm");
-    const combinedFormModal = document.getElementById("combinedFormModal");
+        document.getElementById("contactPage").addEventListener("click", function () {
+            navigateTo("contactpage");
+        });
+    }
 
-    toggleFormButton.addEventListener("click", function () {
-        if (combinedFormModal.style.display === "none" || combinedFormModal.style.display === "") {
-            combinedFormModal.style.display = "block";
-        } else {
-            combinedFormModal.style.display = "none";
-        }
-    });
-
-    // Handle click on login/sign-up button
-    const loginSignupButton = document.getElementById("toggleForm");
-
-    loginSignupButton.addEventListener("click", function (e) {
-        e.preventDefault();
-        // Add your logic for handling the click event here
-        console.log("Login/Sign-up button clicked!");
+    const toggleForms = () => {
         const registrationForm = document.getElementById("registration");
+        const combinedFormModal = document.getElementById("combinedFormModal");
+
         registrationForm.classList.toggle("form--hidden");
+        combinedFormModal.classList.toggle("form--hidden");
+    };
+
+    const closeModal = () => {
+        const combinedFormModal = document.getElementById("combinedFormModal");
+        combinedFormModal.style.display = "none";
+    };
+
+    const showCombinedForm = () => {
+        const combinedFormModal = document.getElementById("combinedFormModal");
+        combinedFormModal.style.display = "block";
+    };
+
+    // Handle click on Log In/Sign Up link
+    const toggleFormLink = document.getElementById("toggleForm");
+    toggleFormLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        toggleForms();
     });
+
+    // Handle click on close modal button
+    const closeModalButton = document.getElementById("closeModal");
+    closeModalButton.addEventListener("click", function () {
+        closeModal();
+    });
+
+    // Handle click on show combined form button
+    const toggleFormButton = document.getElementById("toggleForm");
+    toggleFormButton.addEventListener("click", function () {
+        showCombinedForm();
+    });
+
+    addSmoothScrolling();
+    setupNavigationLinks();
 });
