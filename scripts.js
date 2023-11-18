1bc7c766509be98946151df50360ed4c61463727
Jovaun_Henriques <jovaun@hotmail.com>
2023-11-17 22:40:12 -0330

    edits

--- scripts.js
+++ scripts.js
@@ -1,32 +1,21 @@
-function showLoginPopup() {
-    let form = document.getElementById("registration");
-    form.classList.remove("form--hidden");
-}
-
-function toggleForm() {
-    let form_2 = document.getElementById("combinedFormModal");
-    form_2.style.display = "block";
-    
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-    function addSmoothScrolling() {
-        document.querySelectorAll('a').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
+    // function addSmoothScrolling() {
+    //     document.querySelectorAll('a').forEach(anchor => {
+    //         anchor.addEventListener('click', function (e) {
+    //             e.preventDefault();
 
-                const targetId = this.getAttribute('href').substring(1);
-                const targetElement = document.getElementById(targetId);
+    //             const targetId = this.getAttribute('href').substring(1);
+    //             const targetElement = document.getElementById(targetId);
 
-                if (targetElement) {
-                    window.scrollTo({
-                        top: targetElement.offsetTop - 50,
-                        behavior: 'smooth'
-                    });
-                }
-            });
-        });
-    }
+    //             if (targetElement) {
+    //                 window.scrollTo({
+    //                     top: targetElement.offsetTop - 50,
+    //                     behavior: 'smooth'
+    //                 });
+    //             }
+    //         });
+    //     });
+    // }
 
     // Event listeners for navigation links
     function navigateTo(page) {
@@ -53,14 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    const toggleForms = () => {
-        const registrationForm = document.getElementById("registration");
-        const combinedFormModal = document.getElementById("combinedFormModal");
-
-        registrationForm.classList.toggle("form--hidden");
-        combinedFormModal.classList.toggle("form--hidden");
-    };
-
+    
     const closeModal = () => {
         const combinedFormModal = document.getElementById("combinedFormModal");
         combinedFormModal.style.display = "none";
@@ -72,24 +54,40 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     // Handle click on Log In/Sign Up link
-    const toggleFormLink = document.getElementById("toggleForm");
+    const toggleFormLink = document.getElementById("openLogin");
     toggleFormLink.addEventListener("click", function (e) {
         e.preventDefault();
-        toggleForms();
+        const registrationForm = document.getElementById("registration");
+        registrationForm.classList.toggle("form--hidden");
     });
 
+     // Handle click on show combined form button
+    // const toggleFormButton = document.getElementById("toggleForm");
+    // toggleFormButton.addEventListener("click", function () {
+    //     showCombinedForm();
+    // });
     // Handle click on close modal button
     const closeModalButton = document.getElementById("closeModal");
     closeModalButton.addEventListener("click", function () {
         closeModal();
     });
-
-    // Handle click on show combined form button
-    const toggleFormButton = document.getElementById("toggleForm");
-    toggleFormButton.addEventListener("click", function () {
-        showCombinedForm();
-    });
-
-    addSmoothScrolling();
     
+    // addSmoothScrolling();
 });
+// function setInputError(inputElement, message) {
+//     inputElement.classList.add("form__input--error");
+//     inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+// }
+
+// function clearInputError(inputElement) {
+//     inputElement.classList.remove("form__input--error");
+//     inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+// }
+function toggleForms () {
+    const registrationForm = document.getElementById("registration");
+    const combinedFormModal = document.getElementById("combinedFormModal");
+
+    registrationForm.classList.toggle("form--hidden");
+    combinedFormModal.classList.toggle("form--hidden");
+    
+};
